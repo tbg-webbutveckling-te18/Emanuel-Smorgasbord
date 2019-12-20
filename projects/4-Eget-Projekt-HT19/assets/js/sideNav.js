5a9c8a44c5d25b1cde6ef0a0f2b5ccd2f9bdddee
REKTorn <emanuel.carlhed@gmail.com>
2019-12-20 14:17:13 +0100

    Mobile Side Navigation Animation Complete

--- projects/4-Eget-Projekt-HT19/assets/js/sideNav.js
+++ projects/4-Eget-Projekt-HT19/assets/js/sideNav.js
@@ -1,18 +1,39 @@
 // Open and Close SideNav on Button
-function openSideNav(){
-    document.getElementById("sideNav").style.transform = "translateX(256px)"
-}
+window.addEventListener('load',function(){
+	var slider = document.getElementById("sideButton");
+    slider.addEventListener("click", sideNav);
+});
 
-function closeSideNav(){
-    document.getElementById("sideNav").style.transform = "translateX(0px)"
+function sideNav(){
+    var slideNav = document.getElementById("sideNav");
+    slideNav.classList.toggle("slideNav");
+
+    var top = document.getElementById("burgerTop");
+    top.classList.toggle("burgerTop");
+
+    var mid = document.getElementById("burgerMid");
+    mid.classList.toggle("burgerMid");
+
+    var bot = document.getElementById("burgerBot");
+    bot.classList.toggle("burgerBot");
 }
 
 //Close SideNav on CLick Outside
 document.addEventListener("click", handleMousePos);
 
 function handleMousePos(event) {
-  var mouseClickWidth = event.clientX;
-  if(mouseClickWidth >= 256){
-    document.getElementById("sideNav").style.transform = "translateX(0px)"
-  }
+    var mouseClickWidth = event.clientX;
+    if(mouseClickWidth >= 256){
+        var slideNav = document.getElementById("sideNav");
+        slideNav.classList.remove("slideNav");
+
+        var top = document.getElementById("burgerTop");
+        top.classList.remove("burgerTop");
+
+        var mid = document.getElementById("burgerMid");
+        mid.classList.remove("burgerMid");
+
+        var bot = document.getElementById("burgerBot");
+        bot.classList.remove("burgerBot");
+    }
 }
