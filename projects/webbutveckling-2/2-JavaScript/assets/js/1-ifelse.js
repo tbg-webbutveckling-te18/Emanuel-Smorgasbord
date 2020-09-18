9349c374788a7037477da37f1be4e9e327a43c85
REKTorn <emanuel.carlhed@gmail.com>
2020-09-18 15:12:17 +0200

    Started JS

---
+++ projects/webbutveckling-2/2-JavaScript/assets/js/1-ifelse.js
@@ -0,0 +1,28 @@
+// Greater than
+var age = 19;
+if (age >= 20) {
+    console.log('Du får gå på systembolaget.');
+}
+else {
+    console.log();
+}
+
+// If something is equal too
+var lession = 'Web development';
+if (lession == 'History') {
+    console.log('We have: ' + lession);
+}
+else if (lession == 'Web development') {
+    console.log('We have: ' + lession)
+}
+else {
+    console.log('Spare time!')
+}
+
+var isSunny = false;
+if (isSunny == true) {
+    console.log('Put on some shorts today.')
+}
+else {
+    console.log('Put on some warm clothes today.')
+}
