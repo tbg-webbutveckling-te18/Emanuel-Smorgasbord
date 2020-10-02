0aa12b9ffaca8da52a9a59607dc4d2d6124db672
REKTorn <emanuel.carlhed@gmail.com>
2020-10-02 15:11:36 +0200

    Continued For Loops

--- projects/webbutveckling-2/2-JavaScript/assets/js/1-ifelse.js
+++ projects/webbutveckling-2/2-JavaScript/assets/js/1-ifelse.js
@@ -2,8 +2,7 @@
 var age = 19;
 if (age >= 20) {
     console.log('Du får gå på systembolaget.');
-}
-else {
+} else {
     console.log();
 }
 
@@ -11,55 +10,52 @@ else {
 var lession = 'Web development';
 if (lession == 'History') {
     console.log('We have: ' + lession);
-}
-else if (lession == 'Web development') {
-    console.log('We have: ' + lession)
-}
-else {
-    console.log('Spare time!')
+} else if (lession == 'Web development') {
+    console.log('We have: ' + lession);
+} else {
+    console.log('Spare time!');
 }
 
 var isSunny = false;
 if (isSunny == true) {
-    console.log('Put on some shorts today.')
-}
-else {
-    console.log('Put on some warm clothes today.')
+    console.log('Put on some shorts today.');
+} else {
+    console.log('Put on some warm clothes today.');
 }
 
 // Step 1
-var d = new Date()
+var d = new Date();
 
 if (d.getHours() < 18) {
-    console.log('Good Day!')
+    console.log('Good Day!');
 } else {
-    console.log('Good Ebening!')
+    console.log('Good Ebening!');
 }
 
 // Step 2
 if (d.getHours() < 10) {
-    console.log('Good morning!')
+    console.log('Good morning!');
 } else if (d.getHours() < 18) {
-    console.log('Good day!')
+    console.log('Good day!');
 } else {
-    console.log('Good ebening!')
+    console.log('Good ebening!');
 }
 
 // Step 3
 if (d.getHours() < 10) {
     if (d.getMonth() > 9 || d.getMonth() < 2) {
-        console.log('Go ahead and sleep like a bear!')
+        console.log('Go ahead and sleep like a bear!');
     } else {
-        console.log('Good morning!')
+        console.log('Good morning!');
     }
 } else if (d.getHours() < 18) {
-    console.log('Good day!')
+    console.log('Good day!');
 } else {
-    console.log('Good ebening!')
+    console.log('Good ebening!');
 }
 
 var grade = 'A';
-switch(grade) {
+switch (grade) {
     case 'A':
         console.log('You got an ' + grade + ', Great Job!');
         break;
@@ -79,29 +75,29 @@ switch(grade) {
         console.log('You got an ' + grade + ', Great Job!');
         break;
     default:
-        console.log('Unknown grade.')
+        console.log('Unknown grade.');
 }
 
-switch(d.getDay()) {
+switch (d.getDay()) {
     case 0:
-        console.log('It is Sunday!')
+        console.log('It is Sunday!');
         break;
     case 1:
-        console.log('It is Monday!')
+        console.log('It is Monday!');
         break;
     case 2:
-        console.log('It is Tuesday!')
+        console.log('It is Tuesday!');
         break;
     case 3:
-        console.log('It is Wednesday!')
+        console.log('It is Wednesday!');
         break;
     case 4:
-        console.log('It is Thursday!')
+        console.log('It is Thursday!');
         break;
     case 5:
-        console.log('It is Friday!')
+        console.log('It is Friday!');
         break;
     case 6:
-        console.log('It is Saturday!')
+        console.log('It is Saturday!');
         break;
 }
--- projects/webbutveckling-2/2-JavaScript/assets/js/2-for.js
+++ projects/webbutveckling-2/2-JavaScript/assets/js/2-for.js
@@ -1,7 +1,35 @@
-for(var i = 0; i < 10; i++) {
-    document.writeln('Count is ' + i + '<br>')
+for (var i = 0; i < 10; i++) {
+    document.writeln('Count is ' + i + '<br>');
 }
 
-for(var count = 1; count < 13; count + 2) {
-    document.writeln('Count is ' + i + '<br>')
+for (var count = 1; count < 13; count += 2) {
+    document.writeln('Count is ' + i + '<br>');
+}
+
+// Exercise 3
+
+for (var i = -10; i < 20; i++) {
+    document.writeln(i);
+}
+
+// Exercise 4
+
+for (var i = 10; i % 2 == 0 && i < 41; i++) {
+    document.writeln(i);
+}
+
+// Nested for loops
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+document.writeln('<br>');
+
+for (var i = 0; i < 5; i++) {
+    for (var j = 0; j < 5; j++) {
+        document.writeln('*');
+    }
+    document.writeln('<br>');
 }
