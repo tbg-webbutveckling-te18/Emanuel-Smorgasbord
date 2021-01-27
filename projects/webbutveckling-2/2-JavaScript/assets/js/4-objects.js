a3f45ab54079d8fe02bc067996156ac152ba0158
REKTorn <emanuel.carlhed@gmail.com>
2021-01-27 11:56:30 +0100

    More Object Management

--- projects/webbutveckling-2/2-JavaScript/assets/js/4-objects.js
+++ projects/webbutveckling-2/2-JavaScript/assets/js/4-objects.js
@@ -22,4 +22,39 @@ document.writeln(
     `<br></br>My name is ${user.name} and I am ${user.age} years old. I live in ${user.city}<br>I have no friends.`
 );
 
-user.shoutout();
+// user.shoutout();
+
+for (var i = 0; i < user.friends.length; i++) {
+    console.log(user.friends[i]);
+}
+
+// use forEach if array
+user.friends.forEach(function (friend) {
+    console.log(friend);
+});
+
+console.log(user.age);
+user.age++;
+console.log(user.age);
+
+var post = [
+    {
+        author: 'Dennis',
+        comment: 'Hey catz rules!',
+    },
+    {
+        author: 'Arnold',
+        comment: 'Nowai dude! Dogz rules!',
+    },
+    {
+        author: 'Kamala',
+        comment: 'Calm down. Both are equally nice.',
+    },
+];
+
+// HTML Reference
+var postOutput = document.querySelector('.post');
+
+post.forEach(function (post) {
+    postOutput.innerHTML += `Author: ${post.author} <br> Commented: ${post.comment} <br><br>`;
+});
