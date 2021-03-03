dcb0e89a68576f6b60523a9b02f894cdd15e3f1e
REKTorn <emanuel.carlhed@gmail.com>
2021-03-03 18:50:00 +0100

    GA Work

--- projects/webbutveckling-2/egna-projects/GA-Projekt/assets/js/slideNav.js
+++ projects/webbutveckling-2/egna-projects/GA-Projekt/assets/js/slideNav.js
@@ -1,8 +1,19 @@
 /* Source: http://jsfiddle.net/onigetoc/mk4uF/ */
 
-$('#slideBtn').click(function () {
-    $('.navigation').toggleClass('slideNav');
-    $('.toggleBurgerTop').toggleClass('burgerTop');
-    $('.toggleBurgerMid').toggleClass('burgerMid');
-    $('.toggleBurgerBot').toggleClass('burgerBot');
+/* ------------- jQuery ------------- */
+
+// $('.slideBtn').click(function () {
+//     $('.navigation').toggleClass('slideNav');
+//     $('.toggleBurgerTop').toggleClass('burgerTop');
+//     $('.toggleBurgerMid').toggleClass('burgerMid');
+//     $('.toggleBurgerBot').toggleClass('burgerBot');
+// });
+
+/* ------------- Vanilla JavaScript ------------- */
+
+document.querySelector('.slideBtn').addEventListener('click', () => {
+    document.querySelector('.navigation').classList.toggle('slideNav');
+    document.querySelector('.toggleBurgerTop').classList.toggle('burgerTop');
+    document.querySelector('.toggleBurgerMid').classList.toggle('burgerMid');
+    document.querySelector('.toggleBurgerBot').classList.toggle('burgerBot');
 });
